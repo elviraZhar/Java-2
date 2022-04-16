eaf3465ca42c57cf6655844211e2a08f8922ebe2
ElviraZharenkova <elvira_bitl@list.ru>
2022-04-16 16:19:00 +0800

    lesson 3 Список Телефонный справочник

--- src/main/java/lesson3/MainApp3.java
+++ src/main/java/lesson3/MainApp3.java
@@ -3,4 +3,42 @@ package lesson3;
 import java.util.*;
 
 public class MainApp3 {
- 
+    static class Metal {
+        String name;
+        Metal (String name) {this.name = name;}
+
+        @Override
+        public String toString() {
+            return "Metal{" + "name='" + name + '\'' + '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Metal metal = (Metal) o;
+            return Objects.equals(name, metal.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+
+    public static void main (String [] args) {
+        Metal[] metals = {new Metal("brass"), new Metal("bronze"), new Metal("silver"),new Metal("copper"), new Metal("brass"),
+                new Metal("gold"), new Metal("iron"), new Metal("mercury"), new Metal("copper"),
+                new Metal("lead"), new Metal("nickel"), new Metal("steel"), new Metal("tin")};
+
+        List<Metal> arrMetal = new ArrayList<>(Arrays.asList(metals));
+        System.out.println(arrMetal);
+
+        HashSet<Metal> setMetal = new HashSet<>(Arrays.asList(metals));
+        System.out.println(setMetal);
+        System.out.println("Количество уникальных металлов " + (setMetal.size()));
+
+    }
+}
+
+
--- src/main/java/lesson3/PhoneBook.java
+++ src/main/java/lesson3/PhoneBook.java
@@ -4,4 +4,23 @@ import java.util.*;
 
 public class PhoneBook {
 
+    public static void main (String [] args) {
+        List<String> ivanov = new ArrayList<>();
+        List<String> petrov = new ArrayList<>();
+        List<String> glebova = new ArrayList<>();
+
+        ivanov.add(0, "9991234578");
+        ivanov.add(1, "9644562378");
+        petrov.add(0,"9249876521");
+        petrov.add(1,"9994563917");
+        glebova.add(0,"9999631485");
+
+        HashMap<String, List<String>> phoneBook = new HashMap<>();
+        phoneBook.put("Ivanov", (List<String>) ivanov);
+        phoneBook.put("Petrov", (List<String>) petrov);
+        phoneBook.put("Glebova", (List<String>) glebova);
+
+        System.out.println(phoneBook);
+    }
+
 }
