3465d0476ec29ad5013edea710b4767eed55db6f
ElviraZharenkova <elvira_bitl@list.ru>
2022-04-17 18:11:36 +0800

    lesso 2 исправления

--- src/main/java/lesson2/MainApp2.java
+++ src/main/java/lesson2/MainApp2.java
@@ -2,43 +2,40 @@ package lesson2;
 
 import java.util.Arrays;
 
-//public class MainApp2 {
-//    public static void main (String [] args) throws Throwable {
-//    class MyArraySizeException extends Throwable{
-//        MyArraySizeException(String s) {
-//            super(s);
-  //      }
-//    }
- //   class MyArrayDataExceotion extends Throwable{
- //       MyArrayDataExceotion(int i, int j) {
-  //              super();
-   //     }
-  //  }
+public class MainApp2 {
+    static class MyArraySizeException extends Throwable{
+        MyArraySizeException(String s) {
+            super(s);
+        }
+    }
+    static class MyArrayDataExceotion extends Throwable{
+        MyArrayDataExceotion(int i, int j) {
+            super();
+        }
+    }
+   public static void main (String [] args) throws Throwable {
+        String[][] arr = {{"1","2","3","4"}, {"5","6","7","8"}, {"9","16","10","11"}, {"12","13","14","15"}};
+        int [][] arrI = new int[4][4];
+        int sum = 0;
 
- //   public void changeArray(String[][] arr){
+        if (arr.length != 4) {
+            throw new MyArraySizeException("Неверное количество строк");
+        }
+            for (int i = 0; i <arr.length; i++) {
+                if (arr[i].length != 4) throw new MyArraySizeException("Неверное количество столбцов");
+                int j;
+                for (j = 0; j < arr.length; j++) {
+                    try {
+                        arrI[i][j] = Integer.parseInt(arr[i][j]);
+                    } catch (NumberFormatException e) {
+                        throw new MyArrayDataExceotion(i,j);
+                    }
+                    System.out.println("Ошибка в поле " + i + j);
+                }
+                sum = sum + arrI[i][j];
+            } System.out.println(sum);
+        }
 
- //       String[][] arr = {{"1","2","3","4"}, {"5","6","7","8"}, {"9","16","10","11"}, {"12","13","14","15"}};
- //       int [][] arrI = new int[4][4];
- //       int sum = 0;
+   }
 
- //       if (arr.length != 4) {
-  //          throw new MyArraySizeException("Неверное количество строк");
-  //          for (int i = 0; i <arr.length; i++) {
-  //              if (arr[i].length != 4) throw new MyArraySizeException("Неверное количество столбцов");
-  //              int j;
-  //              for (j = 0; j < arr.length; j++) {
-  //                  try {
-  //                      arrI[i][j] = Integer.parseInt(arr[i][j]);
-  //                  } catch (NumberFormatException e) {
-  //                      throw new MyArrayDataExceotion(i,j);
-  //                  }
-   //                 System.out.println("Ошибка в поле " + i + j);
-   //             }
-   //             sum = sum + arrI[i][j];
-   //         }
-   //     }
-   //    // System.out.println(sum);
-   // }
-//
-  //  }
-//}
+
