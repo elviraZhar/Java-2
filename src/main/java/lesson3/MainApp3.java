6b2b636ad0d8dfd053c9de9e89c850c23b50bbb1
ElviraZharenkova <elvira_bitl@list.ru>
2022-04-17 20:11:20 +0800

    lesso 2 master

--- src/main/java/lesson3/MainApp3.java
+++ src/main/java/lesson3/MainApp3.java
@@ -5,7 +5,14 @@ import java.util.*;
 public class MainApp3 {
     static class Metal {
         String name;
-        Metal (String name) {this.name = name;}
+
+        Metal(String name) {
+            this.name = name;
+        }
+
+        public Object getName(){
+            return name;
+        }
 
         @Override
         public String toString() {
@@ -27,8 +34,9 @@ public class MainApp3 {
     }
 
     public static void main (String [] args) {
-        Metal[] metals = {new Metal("brass"), new Metal("bronze"), new Metal("silver"),new Metal("copper"), new Metal("brass"),
-                new Metal("gold"), new Metal("iron"), new Metal("mercury"), new Metal("copper"),
+        Metal[] metals = {new Metal("brass"), new Metal("bronze"), new Metal("silver"),
+                new Metal("copper"), new Metal("brass"), new Metal("gold"),
+                new Metal("iron"), new Metal("mercury"), new Metal("copper"),
                 new Metal("lead"), new Metal("nickel"), new Metal("steel"), new Metal("tin")};
 
         List<Metal> arrMetal = new ArrayList<>(Arrays.asList(metals));
@@ -36,6 +44,16 @@ public class MainApp3 {
 
         HashSet<Metal> setMetal = new HashSet<>(Arrays.asList(metals));
         System.out.println(setMetal);
+
+        Map resultMap = new HashMap<>();
+        for (int i = 0; i < arrMetal.size(); i++) {
+            if (resultMap.containsKey(arrMetal.get(i))) {
+                resultMap.put(arrMetal.get(i), resultMap.get(arrMetal.get(i)) + 1);
+            } else {
+                resultMap.put(arrMetal.get(i), 1);
+            }
+        }
+        System.out.println(resultMap);
         System.out.println("Количество уникальных металлов " + (setMetal.size()));
 
     }
