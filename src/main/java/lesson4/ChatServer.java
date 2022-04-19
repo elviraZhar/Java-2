5f02a9d9af1e5051049405c7a9606aaac41b9aef
ElviraZharenkova <elvira_bitl@list.ru>
2022-04-19 22:15:00 +0800

    lesson 4 добавление текста в лог

---
+++ src/main/java/lesson4/ChatServer.java
@@ -0,0 +1,12 @@
+package lesson4;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server started at port " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+//создать файл для хранения сообщений из чата с возможностью дописывать текст
+}
